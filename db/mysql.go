6a787237d411d32bb7a0ed64bc5e446f6991ce4a
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-22 22:25:53 +0700

    feat: setup db

---
+++ db/mysql.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+func ConnectDB(){
+	username := "root"
+	password := ""
+	server := "127.0.0.1:3306"
+	dbName := "product_modul"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",username, password, server, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Failed open DB:", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Database not connected:", err)
+	}
+
+	log.Println("\nDatabse connected!")
+	DB = db
+}

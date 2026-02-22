9e2cab07c5a229a81f808c0af5341b9835c013b4
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-22 22:25:04 +0700

    feat: setup golang

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"pert3_npm/db"
+	"pert3_npm/handlers"
+)
+
+const PORT = 8000 // 4 digit npm belakang
+
+func main(){
+	db.ConnectDB()
+	// Akses halaman home
+	http.HandleFunc("/", handlers.HomeHandler)
+	// akses halaman edit
+	http.HandleFunc("/edit", handlers.EditProductHandler)
+	// update data
+	http.HandleFunc("/update", handlers.UpdateProduct)
+	// create data
+	http.HandleFunc("/create", handlers.CreateProductHandler)
+	// delete data
+	http.HandleFunc("/delete", handlers.DeleteProduct)
+	
+	fmt.Printf("Server berjalan di http://localhost:%d", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT),nil)
+}

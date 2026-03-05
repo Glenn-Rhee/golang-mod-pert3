0ddb276d8a271fb2182449367e6b5465bc675824
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-05 20:21:38 +0700

    refactor: update product handller

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,12 +2,9 @@ package handlers
 
 import (
 	"html/template"
-	"io"
 	"net/http"
-	"net/url"
 	"pert3_npm/db"
 	"pert3_npm/helper"
-	"strconv"
 )
 
 var tmpl *template.Template
@@ -86,7 +83,8 @@ func CreateProductHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func UpdateProductHandler(w http.ResponseWriter, r *http.Request){
+func UpdateProductHandler(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method != http.MethodPost {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
@@ -94,74 +92,53 @@ func UpdateProductHandler(w http.ResponseWriter, r *http.Request){
 
 	err := r.ParseMultipartForm(1 << 20)
 	if err != nil {
-		http.Redirect(w, r, "/?error=File is to large. Max 1MB", http.StatusSeeOther)
+		helper.RedirectError(w, r, "File is too large. Max 1MB")
 		return
 	}
-
-	r.ParseForm()
 
 	id := r.FormValue("id")
 	name := r.FormValue("name")
-	price, err := strconv.ParseInt(r.FormValue("price"), 10, 64)
+
+	price, err := helper.ParseStock(r)
 	if err != nil {
-		errMsg := url.QueryEscape("Price must be a number")
-		http.Redirect(w, r, "/?error=" + errMsg, http.StatusSeeOther)
+		helper.RedirectError(w, r, "Price must be a number")
 		return
 	}
-	stock, err := strconv.ParseInt(r.FormValue("stock"), 10, 64)
+
+	stock, err := helper.ParseStock(r)
 	if err != nil {
-		errMsg := url.QueryEscape("Stock must be a number")
-		http.Redirect(w, r, "/?error=" + errMsg, http.StatusSeeOther)
+		helper.RedirectError(w, r, "Stock must be a number")
 		return
 	}
 
 	isActive := r.FormValue("is_active") == "on"
-	var imgFile []byte
+
+	var imgBytes []byte
+
 	file, _, err := r.FormFile("image")
 
 	if err == nil {
-		defer file.Close()
-
-		// Membaca file gambar, apabila ada error mengembalikan response
-		fileBytes, err := io.ReadAll(file)
+		imgBytes, err = helper.ReadAndValidateImage(file)
 		if err != nil {
-			http.Redirect(w, r, "/?error=Failed read file", http.StatusInternalServerError)
+			helper.RedirectError(w, r, err.Error())
 			return
 		}
-
-		// Mengecek apakah ukuran gambar itu lebih kecil dari 1mb
-		if len(fileBytes) > 1<<20 {
-			http.Redirect(w, r, "?error=File size more than 1MB", http.StatusSeeOther)
-			return
-		}
-
-		// Mengecek tipe file apakah jpeg atau png, jika bukan akan mengembalikan error
-		fileType := http.DetectContentType(fileBytes)
-		if fileType != "image/jpeg" && fileType != "image/png" {
-			http.Redirect(w, r, "?error=JPEG or PNG type only", http.StatusSeeOther)
-			return
-		}
-
-		imgFile = fileBytes
 	}
-	
+
 	var query string
 
-	if imgFile != nil {
+	if imgBytes != nil {
 		query = `
-			UPDATE products 
-			SET name=?, price=?, stock=?, is_active=?, image=? 
-			WHERE id=?`
+		UPDATE products 
+		SET name=?, price=?, stock=?, is_active=?, image=? 
+		WHERE id=?`
+		_, err = db.DB.Exec(query, name, price, stock, isActive, imgBytes, id)
+
 	} else {
 		query = `
-			UPDATE products 
-			SET name=?, price=?, stock=?, is_active=? 
-			WHERE id=?`
-	}
-	
-	if imgFile != nil {
-		_, err = db.DB.Exec(query, name, price, stock, isActive, imgFile, id)
-	} else {
+		UPDATE products 
+		SET name=?, price=?, stock=?, is_active=? 
+		WHERE id=?`
 		_, err = db.DB.Exec(query, name, price, stock, isActive, id)
 	}
 

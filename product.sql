41e7438a28f0a4b604fb372633dad0adb1967e2e
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-08 21:34:54 +0700

    refactor: added product_details table

--- product.sql
+++ product.sql
@@ -7,3 +7,17 @@ CREATE TABLE products (
     name    VARCHAR(255) NOT NULL,
     price   DECIMAL(15,2) NOT NULL
 );
+
+-- Buat Table product_details
+CREATE TABLE product_details (
+    product_id  VARCHAR(100) PRIMARY KEY,
+    stock       INT NOT NULL,
+    is_active   BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    image       LONGBLOB,
+    
+    CONSTRAINT fk_product_details_product
+        FOREIGN KEY (product_id) REFERENCES products(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);

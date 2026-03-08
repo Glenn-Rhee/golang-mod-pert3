a5d1199cdd59dfdd063d38ac0e983b7fda08ff00
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-08 21:34:31 +0700

    feat: remove column at products table

--- product.sql
+++ product.sql
@@ -3,11 +3,7 @@ CREATE DATABASE TokoLepkom_npm;
 
 -- Buat Table Products nya
 CREATE TABLE products (
-    id          VARCHAR(100) PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    price       DECIMAL(15,2) NOT NULL,
-    stock       INT NOT NULL,
-    is_active   BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    image       LONGBLOB
+    id      VARCHAR(100) PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    price   DECIMAL(15,2) NOT NULL
 );

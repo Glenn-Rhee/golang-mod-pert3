6190d8ef7fa9434ee8bb8197a46e09b959ef5591
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-31 10:50:09 +0700

    refactor: update config.go added configuration for campus f8

--- db/config.go
+++ db/config.go
@@ -10,10 +10,18 @@ import (
 
 var DB *sql.DB
 func ConnectDB(){
-	username := "root" // Sesuaikan dengan username di Komputer Anda
-	password := "" // Sesuaikan dengan password di Komputer Anda
-	server := "127.0.0.1:3306" // Sesuaikan dengan servername di Komputer Anda
+	/* KONEKSI DI BAWAH UNTUK KAMPUS F4 */
+	username := "root" 
+	password := "" 
+	server := "127.0.0.1:3306" 
 	dbName := "tokolepkom_npm"
+
+	/* KONEKSI DI BAWAH UNTUK KAMPUS f8 */
+	username := "APCx" // Ganti nilai x dengan no PC masing - masing 
+	password := "lepkom@123" 
+	server := "dbms.lepkom.f4.com:3306" 
+	dbName := "tokolepkom_npm"
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",username, password, server, dbName)
 	db, err := sql.Open("mysql", dsn)
 	

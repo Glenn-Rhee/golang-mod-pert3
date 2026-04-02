984659998d698717dc82882e78335c5a94cc56d9
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-02 21:45:38 +0700

    refactor: update config.go

--- db/config.go
+++ db/config.go
@@ -17,10 +17,10 @@ func ConnectDB(){
 	dbName := "tokolepkom_npm"
 
 	/* KONEKSI DI BAWAH UNTUK KAMPUS f8 */
-	username := "APCx" // Ganti nilai x dengan no PC masing - masing 
-	password := "lepkom@123" 
-	server := "dbms.lepkom.f4.com:3306" 
-	dbName := "tokolepkom_npm"
+	// username := "APCx" // Ganti nilai x dengan no PC masing - masing 
+	// password := "lepkom@123" 
+	// server := "dbms.lepkom.f4.com:3306" 
+	// dbName := "tokolepkom_npm"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",username, password, server, dbName)
 	db, err := sql.Open("mysql", dsn)

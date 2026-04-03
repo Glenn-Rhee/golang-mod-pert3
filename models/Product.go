f9745bcde6c0b3d82b79d824b59e59a2dc922072
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-03 20:22:09 +0700

    refactor: update Product.go for praktikan

--- models/Product.go
+++ models/Product.go
@@ -1,14 +1,3 @@
 package models
 
-import "time"
-
-// Buat Struct Product
-type Product struct {
-	Id        string
-	Name      string
-	Price     float64
-	Stock     int
-	IsActive  bool
-	CreatedAt time.Time
-	Image     []byte
-}
+/* TODO - BUAT MODEL PRODUCT */
